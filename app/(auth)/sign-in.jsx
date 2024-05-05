037102ce4b31a546f1056151cc5355247ff0f17c
AhmedEz9 <ahmed.ezzaroui@metropolia.fi>
2024-05-05 22:37:42 +0300

    Added user authentication handling

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,33 +1,34 @@
-import { View, Text, ScrollView, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Link, router } from 'expo-router';
 
 import { images } from '../../constants';
-
 import FormField from '../../components/FormField';
-import CustomButton from '../../components/CustomButton'
-import { signIn } from '../../lib/appwrite';
+import CustomButton from '../../components/CustomButton';
+import { signIn, checkSession } from '../../lib/appwrite';
 
 const SignIn = () => {
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
-
+  const [form, setForm] = useState({ email: "", password: "" });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = async () => {
-    if (!form.email || !form.password) {
-      Alert.alert("Error", "Please fill in all fields");
-    }
+  useEffect(() => {
+    const checkExistingSession = async () => {
+      const session = await checkSession();
+      if (session) router.replace('/home');
+    };
+    checkExistingSession();
+  }, []);
 
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
     setIsSubmitting(true);
     try {
       await signIn(form.email, form.password);
-      // set it to global state...
-
-      router.replace('/home')
+      router.replace('/home');
     } catch (error) {
       Alert.alert("Error", error.message);
     } finally {
@@ -40,36 +41,34 @@ const SignIn = () => {
       <ScrollView>
         <View className="w-full flex justify-center min-h-[80vh] px-4 my-6">
           <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[34px]"/>
-          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">Log in to EcoVoyage</Text>
+          <Text className="text-2xl font-semibold text-white mt-10">Log in to EcoVoyage</Text>
           <FormField 
             title="Email"
             value={form.email}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
+            handleChangeText={(email) => setForm({ ...form, email })}
             otherStyles="mt-7"
             keyboardType="email-address"
           />
-
           <FormField
             title="Password"
             value={form.password}
-            handleChangeText={(e) => setForm({ ...form, password: e })}
+            handleChangeText={(password) => setForm({ ...form, password })}
             otherStyles="mt-7"
           />
           <CustomButton 
-             title="Sign In"
-             handlePress={submit}
-             containerStyles="mt-7"
-             isLoading={isSubmitting}
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
           />
           <View className="flex justify-center pt-5 flex-row gap-2">
-            <Text className="text-lg text-gray-100 font-pregular">Don't have an account?</Text>
-            <Link href="/sign-up" className="text-lg font-psemibold text-secondary">Sign up</Link>
-
+            <Text className="text-lg text-gray-100">Don't have an account?</Text>
+            <Link href="/sign-up" className="text-lg text-secondary">Sign up</Link>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,14 +1,12 @@
-import { View, Text, ScrollView, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Link, router } from 'expo-router';
 
 import { images } from '../../constants';
-
 import FormField from '../../components/FormField';
-import CustomButton from '../../components/CustomButton'
-
-import { createUser } from '../../lib/appwrite';
+import CustomButton from '../../components/CustomButton';
+import { createUser, checkSession } from '../../lib/appwrite';
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -16,20 +14,25 @@ const SignUp = () => {
     email: "",
     password: "",
   });
-
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = async () => {
-    if (!form.username || !form.email || !form.password) {
-      Alert.alert("Error", "Please fill in all fields");
-    }
+  useEffect(() => {
+    const checkExistingSession = async () => {
+      const session = await checkSession();
+      if (session) router.replace('/home');
+    };
+    checkExistingSession();
+  }, []);
 
+  const submit = async () => {
+    if (form.username === "" || form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
     setIsSubmitting(true);
     try {
-      const result = await createUser(form.email, form.password, form.username);
-      // set it to global state...
-
-      router.replace('/home')
+      await createUser(form.email, form.password, form.username);
+      router.replace('/home');
     } catch (error) {
       Alert.alert("Error", error.message);
     } finally {
@@ -42,44 +45,40 @@ const SignUp = () => {
       <ScrollView>
         <View className="w-full flex justify-center min-h-[80vh] px-4 my-6">
           <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[34px]"/>
-          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">Sign up to EcoVoyage</Text>
+          <Text className="text-2xl font-semibold text-white mt-10">Sign up to EcoVoyage</Text>
           <FormField
             title="Username"
             value={form.username}
-            handleChangeText={(e) => setForm({ ...form, username: e })}
+            handleChangeText={(username) => setForm({ ...form, username })}
             otherStyles="mt-10"
           />
-
           <FormField
             title="Email"
             value={form.email}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
+            handleChangeText={(email) => setForm({ ...form, email })}
             otherStyles="mt-7"
             keyboardType="email-address"
           />
-
           <FormField
             title="Password"
             value={form.password}
-            handleChangeText={(e) => setForm({ ...form, password: e })}
+            handleChangeText={(password) => setForm({ ...form, password })}
             otherStyles="mt-7"
           />
-
           <CustomButton 
-             title="Sign Up"
-             handlePress={submit}
-             containerStyles="mt-7"
-             isLoading={isSubmitting}
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
           />
           <View className="flex justify-center pt-5 flex-row gap-2">
-            <Text className="text-lg text-gray-100 font-pregular">Have an account already?</Text>
-            <Link href="/sign-in" className="text-lg font-psemibold text-secondary">Sign in</Link>
-
+            <Text className="text-lg text-gray-100">Have an account already?</Text>
+            <Link href="/sign-in" className="text-lg text-secondary">Sign in</Link>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
