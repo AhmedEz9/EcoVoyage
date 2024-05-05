5237aea72fa17ca893e7b447e47e22a6b04bc050
sohaibebrahimi <ebrahimi_sa@icloud.com>
2024-05-05 12:48:25 +0300

    Added: CommonStyles

---
+++ Ecovoyage/src/commonStyles/colors.ts
@@ -0,0 +1,7 @@
+export const MAIN_color1 = '#FF9519';
+export const MAIN_color2 = '#0455BF';
+export const TEXT_BODY = '#033E8C';
+export const ACTION = '#C96000';
+export const WARNING = '#E83D3D';
+export const BACKGROUND = 'rgba(242, 195, 53, 0.08)';
+export const MAIN_CONTAINER_BG = '#FEFAEF';
