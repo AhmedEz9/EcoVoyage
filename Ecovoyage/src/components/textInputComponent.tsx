83ad3244cfa9f8a318d9d662859187702c3f6564
sohaibebrahimi <ebrahimi_sa@icloud.com>
2024-05-07 17:24:25 +0300

    Added search and homepage

--- Ecovoyage/src/components/textInputComponent.tsx
+++ Ecovoyage/src/components/textInputComponent.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import {SafeAreaView, StyleSheet, TextInput, View,StyleProp, ViewStyle,Button } from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput, View,StyleProp, ViewStyle,Button, TouchableOpacity } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import SearchComponent from './searchComponent';
+import spacing from '../commonStyles/Spacing';
+import { black_100, black_200, white } from '../commonStyles/colors';
 
 
 interface TextInputComponentProps {
@@ -19,16 +21,25 @@ const TextInputComponent: React.FC<TextInputComponentProps> = ({ style })=> {
 
   
   return (
-    <View style={{ flexDirection: 'column', justifyContent: 'space-between' }}>
-      <View style={styles.searchSection}>
-        <Ionicons style={styles.searchIcon} name="search-circle-outline" size={24} color="black" />
+    <View style={{ flexDirection: 'column',alignItems:'center' }}>
+      <View style={{...styles.searchSection, ...spacing.mt2}}>
         <TextInput
           style={styles.input}
-          placeholder="Search for travel locations"
+          placeholder="Enter your budget"
+          placeholderTextColor={white}
           onChangeText={newText => setInputText(newText)}
           value={inputText}
         />
-        <Button title="Search" onPress={handleInput}  />
+      
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={[
+            styles.button,
+          ]}
+          >
+          <Ionicons style={styles.searchIcon} onPress={handleInput} name="search-circle-outline" size={40} color="white" />
+  
+        </TouchableOpacity>
       </View>
 
       <SearchComponent input={input}/>
@@ -40,15 +51,20 @@ const styles = StyleSheet.create({
   searchSection: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    alignContent: 'center',
+    justifyContent:'center',
+    backgroundColor: black_200,
     paddingHorizontal: 10,
-    borderRadius: 5,
-    marginHorizontal: 20, // Adjust this as per your layout
-    marginVertical: 10,   // Adjust this as per your layout
+    borderRadius: 25,
+    marginVertical: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
-    marginTop:150,
-    width: '85%',
+    borderColor: black_100,
+    width:'95%',
+  
+  },
+  button: {
+    flexDirection: 'row',
+    backgroundColor:'transparent',
   },
   searchIcon: {
     padding: 10,
@@ -56,8 +72,11 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     height: 40,
-    width:1000,
+    width:'100%',
+    color: white,
+
   },
+
 });
 
 export default TextInputComponent;
--- Ecovoyage/src/views/homePage.tsx
+++ Ecovoyage/src/views/homePage.tsx
@@ -1,17 +1,41 @@
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { primary } from '../commonStyles/colors';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import TextInputComponent from '../components/textInputComponent';
 //import { TextInputComponent } from '../components/textInputComponent'
 
 export default function HomePage () {
   const travelPackages = require('../../assets/travel.json');
   return (
-    <View >
-      <Text>home Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.menuContainer}>
+        {/* this is menu later name and back button to be added */}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={[
+            styles.button,
+          ]}
+          >
+            <AntDesign name="profile" size={40} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <TextInputComponent/>
+      
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: primary,
+    flex: 1,
+  },
+  menuContainer: {
+    alignItems:'flex-end',
+  },
   itemContainer: {
     padding: 10,
     backgroundColor: '#f0f0f0',
@@ -25,6 +49,14 @@ const styles = StyleSheet.create({
   itemDestination: {
     fontSize: 16,
     color: 'gray',
+  },
+  button: {
+    flexDirection: 'row',
+    width: 40,
+    backgroundColor:'transparent',
+  },
+  children: {
+    justifyContent: 'center',
   },
 });
 
